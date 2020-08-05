4dd40f246522313e8c8c247fd900fedfec77d829
czqzju <czqzju@gmail.com>
2020-08-05 13:15:20 +1200

    Initial commit

---
+++ locators.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+class HomePageLocators(object):
+    """Search locator"""
+    SEARCH_BUTTON = (By.NAME, 'btnK')
+    INPUT_AREA = (By.NAME, 'q')
+    VOICE_BUTTON=(By.XPATH, '//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[3]/div[2]')
+    IMAGE_BUTTON = (By.XPATH, '//*[@id="gbw"]/div/div/div[1]/div[2]/a')
+    IMAGE_INPUT_BUTTON = (By.XPATH, '//*[@id="sbtc"]/div/div[3]/div[2]')
+    IMAGE_URL_INPUT = (By.NAME, 'image_url')
+    IMAGE_SEARCH_BUTTON = (By.XPATH, '//*[@id="aoghAf"]/input')
+    IMAGE_RESULT = (By.XPATH, '//*[@id="Z6bGOb"]/a/img')
+
+class SearchResultsPageLocators(object):
+    """A class for search results locators. All search results locators should come here"""
+    pass
---
+++ page.py
@@ -0,0 +1,75 @@
+from locators import HomePageLocators
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+class BasePage(object):
+
+    def __init__(self, driver):
+        self.driver = driver
+
+
+class HomePage(BasePage):
+
+
+    def is_title_matches(self):
+        """Verifies that the hardcoded text Google appears in page title"""
+        return "Google" in self.driver.title
+
+    def click_search_button(self):
+        """Triggers the search"""
+        element = self.driver.find_element(*HomePageLocators.SEARCH_BUTTON)
+        element.click()
+
+    def input_search_content(self, content):
+        element = self.driver.find_element(*HomePageLocators.INPUT_AREA)
+        element.send_keys(content)
+
+    def voice_input_content(self, content):
+        self.driver.find_element(*HomePageLocators.VOICE_BUTTON).click()
+        content.play()
+
+    def click_image_button(self):
+        element = self.driver.find_element(*HomePageLocators.IMAGE_BUTTON)
+        element.click()
+        try:
+            element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(*HomePageLocators.IMAGE_INPUT_BUTTON)
+            )
+            element.click()
+        except Exception as e:
+            # log the exception e
+            self.driver.quit()
+
+    def image_url_input(self, content):
+        try:
+            element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(*HomePageLocators.IMAGE_URL_INPUT)
+            )
+            element.send_keys(content)
+        except Exception as e:
+            # log the exception e
+            self.driver.quit()
+
+    def image_search_click(self):
+        element = self.driver.find_element(*HomePageLocators.IMAGE_SEARCH_BUTTON)
+        element.click()
+
+
+
+class SearchResultsPage(BasePage):
+    """Search results page action methods come here"""
+
+    def check_search_result(self, content):
+
+        return content in self.driver.page_source
+
+    def check_image_search_result(self):
+        try:
+            element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(*HomePageLocators.IMAGE_RESULT)
+            )
+            return True
+        except Exception as e:
+            # log the exception e
+            return False
